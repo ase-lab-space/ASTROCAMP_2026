03cfa5c71971cd5f9bcd78c1752ebb05608f65c2
Yukihisa-Imaizumi <s2312328@u.tsukuba.ac.jp>
2026-02-14 17:47:40 +0900

    New記事を編集可能に

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Top from './pages/Top';
 import SatelliteDev from './pages/SatelliteDev';
 import SatelliteData from './pages/SatelliteData';
 import SpaceBusiness from './pages/SpaceBusiness';
+import NewsPage from './pages/NewsPage';
+import NewsDetailPage from './pages/NewsDetailPage';
 import './App.css';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           <Route path="/satellite-dev" element={<SatelliteDev />} />
           <Route path="/satellite-data" element={<SatelliteData />} />
           <Route path="/space-business" element={<SpaceBusiness />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/news/:slug" element={<NewsDetailPage />} />
         </Routes>
       </div>
     </Router>
